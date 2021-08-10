04995c4c6930a09ce472d8cd1b3bfc67fa319851
Raphael Gregg <raphaelgregg@gmail.com>
2021-08-10 18:03:41 -0300

    feat: creted screen home

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { Home } from './src/screen/Home';
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <Home />
+    </>
+  );
+}
